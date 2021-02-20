dd42ef3bbae2c050be6f28310d37bb6eb55a6034
Mert Akyazı <32460871+Containability@users.noreply.github.com>
2021-02-20 18:41:48 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,34 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+def primeFirst(val):
+    count=0
+    flag=True
+    
+    if val<=500: #500den küçükse
+        for a in range(2,val):
+            if(val%a==0):#kendisinden küçük herhangi bir sayıya bölünüyorsa
+            
+                flag=False#false
+        if flag==True:#Eğer bölünmediyse true bu da asal sayı demek
+            if val==499: #Prime second ile ayrımı olması için son asal sayıda new line
+                print(val,end="\n")
+            else:
+                print(val,end=" ")
+
+def primeSecond(val):		
+		#Aynısının 500den büyük sayılara yapılan hali
+    count=0
+    flag=True
+   
+    if val>500 and val<1000:
+        for a in range(2,val):
+            if(val%a==0):
+            
+                flag=False
+        if flag==True:
+            print(val,end=" ")
+            
+for x in range(1000): #FOR DÖNGÜSÜ
+    primeFirst(x)
+    primeSecond(x)
